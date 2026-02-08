3c2ccc59ac3630e71783cc837509547e035a2f63
liuzhen932 <liuzhen9320@5.1.e.7.0.a.a.e.0.a.2.ip6.arpa>
2026-02-08 12:39:58 +0800

    fix: ai shabi

--- cmd/service/internal/sessions/sessions.go
+++ cmd/service/internal/sessions/sessions.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/jms-guy/timekeep/internal/database"
 	"github.com/jms-guy/timekeep/internal/repository"
-	"golang.org/x/sys/windows"
 )
 
 type Tracked struct {
@@ -207,26 +206,4 @@ func (sm *SessionManager) ValidateActiveSessions(ctx context.Context, logger *lo
 	}
 }
 
-// isProcessRunning checks if a process with the given PID is still running on Windows
-func isProcessRunning(pid int) bool {
-	// On Windows, use OpenProcess to check if the process exists
-	// This is more reliable than Signal(0)
-	handle, err := windows.OpenProcess(windows.PROCESS_QUERY_INFORMATION, false, uint32(pid))
-	if err != nil {
-		// Process doesn't exist or can't be opened
-		return false
-	}
-	defer windows.CloseHandle(handle)
 
-	// Check if process has exited using GetExitCodeProcess
-	var exitCode uint32
-	err = windows.GetExitCodeProcess(handle, &exitCode)
-	if err != nil {
-		// Error getting exit code means process is gone
-		return false
-	}
-
-	// If exit code is STILL_ACTIVE (259), process is still running
-	// Otherwise it has exited
-	return exitCode == 259
-}
